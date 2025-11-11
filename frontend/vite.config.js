e56154e6ff6b88379f28ff044ba2bea190c10715
ishant sharma <rasp.ishantsharma@gmail.com>
2025-11-11 23:46:16 +0530

    fix: proper _redirects placement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      // Add hook to copy _redirects after build
-      plugins: [
-        {
-          name: "copy-redirects",
-          closeBundle() {
-            copyFileSync("public/_redirects", "dist/_redirects");
-          },
-        },
-      ],
-    },
+    outDir: "dist",
   },
+  publicDir: "public",
 });
