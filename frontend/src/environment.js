b2ef5986abc7caddd176ad01281b7c7bb91a66ce
ishant sharma <rasp.ishantsharma@gmail.com>
2025-11-11 05:43:19 +0530

    updating env file

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://nexusbackend-15il.onrender.com/"
+  ? "https://nexusbackend-15il.onrender.com"
   : "http://localhost:8000";
 
 export default server;
